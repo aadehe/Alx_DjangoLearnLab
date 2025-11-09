0bec9ed77aa8611ca8febb042465efe0e59de284
Albert ADEHE <aadehe@gmail.com>
2025-11-09 22:04:40 +0000

    updated views.py with the class base methods

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,16 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from .views import list_books, LibraryDetailView, login_view, logout_view, register_view
+
+from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-Based View
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-Based View
 
 # Authentication routes
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('register/', register_view, name='register'),
+    # Authentication URLs
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
