e4a2387ce7e549b6a714873f025a3d76dda44eda
Albert ADEHE <aadehe@gmail.com>
2025-11-09 20:35:16 +0000

    updated view.py with import statement

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from models import Book, Library
+from .models import Book
+from .models import Library
+
 
 # Create your views here.
-
 # Function-Based View: List all books
 def list_books(request):
     books = Book.objects.all()
