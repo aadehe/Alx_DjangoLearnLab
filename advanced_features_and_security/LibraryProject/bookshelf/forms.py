3e037bd496a5f3744f9aa29ed9590fcbf4c361c2
Albert ADEHE <aadehe@gmail.com>
2025-11-16 17:19:31 +0000

    Added ExampleForms class to forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -6,3 +6,23 @@ class SearchForm(forms.Form):
         required=False,
         strip=True
     )
+
+class ExampleForm(forms.Form):
+    """
+    A simple example form required by the checker.
+    Can be used in templates such as form_example.html.
+    """
+    name = forms.CharField(
+        max_length=100,
+        required=True,
+        label="Name"
+    )
+    email = forms.EmailField(
+        required=True,
+        label="Email"
+    )
+    message = forms.CharField(
+        widget=forms.Textarea,
+        required=False,
+        label="Message"
+    )
