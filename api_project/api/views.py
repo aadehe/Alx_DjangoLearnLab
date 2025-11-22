e8019bf22973495ee06f4ea96109b28bd549b204
Albert ADEHE <aadehe@gmail.com>
2025-11-22 15:33:22 +0000

    Extended the existing view setup by adding a new class BookViewSet

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -8,4 +8,12 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    
 
