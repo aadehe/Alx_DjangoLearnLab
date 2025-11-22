082737215bd9a0f1aa7c15d2c5227a3719cf2b23
Albert ADEHE <aadehe@gmail.com>
2025-11-22 05:10:12 +0000

    added urlpatterns and proper imports

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
+
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 
-
-# Create your views here.
 
 class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
