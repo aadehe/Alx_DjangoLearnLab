e5e34de0dec2e2efa1ee816cc62ac6d6cc3f884a
Albert ADEHE <aadehe@gmail.com>
2025-11-22 04:54:51 +0000

    creaed the api view

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+
 
 # Create your views here.
+
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
